05481aea81a218ca2882a5e2876216f4f3b0e64f
yupingtao <626683392@qq.com>
2020-02-15 19:51:39 +0800

    斐波拉切数计算 1.When 位数为1, when 求值, Then 返回1

---
+++ src/main/java/cc/xpbootcamp/warmup/fibonacci/CalculationUtils.java
@@ -0,0 +1,15 @@
+package cc.xpbootcamp.warmup.fibonacci;
+
+public class CalculationUtils {
+
+    /**
+     * Fibonacci calculation tools
+     * @param number
+     * @return
+     */
+    public static int fibonacci(int number){
+        return 1;
+    }
+
+
+}
