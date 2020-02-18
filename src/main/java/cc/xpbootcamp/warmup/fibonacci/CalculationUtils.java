626315f14b15e534be754161497d6240acfe854a
yupingtao <626683392@qq.com>
2020-02-18 20:15:25 +0800

    完善斐波拉切单元测试 优化斐波拉切性能

--- src/main/java/cc/xpbootcamp/warmup/fibonacci/CalculationUtils.java
+++ src/main/java/cc/xpbootcamp/warmup/fibonacci/CalculationUtils.java
@@ -7,13 +7,19 @@ public class CalculationUtils {
      * @param number
      * @return
      */
-    public static int fibonacci(int number){
-        if (number < 1){
-            throw new IllegalArgumentException("fibonacci cannot calculate data less than 1");
+    public static long fibonacci(long number){
+        if (number < 1 || number > 50){
+            throw new IllegalArgumentException("position is out of bounding");
         } else if (number == 1 || number == 2){
             return 1;
         } else {
-            return fibonacci(number - 1) + fibonacci(number - 2);
+            long tempOne = 1,tempTwo = 1,result = 0;
+            for (int i = 3; i <= number; i++) {
+                result = tempOne + tempTwo;
+                tempTwo = tempOne;
+                tempOne = result;
+            }
+            return result;
         }
     }
 
--- src/test/java/cc/xpbootcamp/warmup/fibonacci/FibonacciTest.java
+++ src/test/java/cc/xpbootcamp/warmup/fibonacci/FibonacciTest.java
@@ -8,45 +8,51 @@ public class FibonacciTest {
 
     @Test
     public void should_return_1_when_calculate_given_position_is_1(){
-        int number = 1;
-        int fibonacci = CalculationUtils.fibonacci(number);
+        long number = 1;
+        long fibonacci = CalculationUtils.fibonacci(number);
         Assert.assertEquals(1,fibonacci);
     }
 
     @Test
     public void should_return_1_when_calculate_given_position_is_2(){
-        int number = 2;
-        int fibonacci = CalculationUtils.fibonacci(number);
+        long number = 2;
+        long fibonacci = CalculationUtils.fibonacci(number);
         Assert.assertEquals(1,fibonacci);
     }
 
     @Test
     public void should_return_2_when_calculate_given_position_is_3(){
-        int number = 3;
-        int fibonacci = CalculationUtils.fibonacci(number);
+        long number = 3;
+        long fibonacci = CalculationUtils.fibonacci(number);
         Assert.assertEquals(2,fibonacci);
     }
 
 
-    @Test
-    public void should_return_3_when_calculate_given_position_is_4(){
-        int number = 4;
-        int fibonacci = CalculationUtils.fibonacci(number);
-        Assert.assertEquals(3,fibonacci);
-    }
-
 
     @Test
     public void should_return_5_when_calculate_given_position_is_5(){
-        int number = 5;
-        int fibonacci = CalculationUtils.fibonacci(number);
+        long number = 5;
+        long fibonacci = CalculationUtils.fibonacci(number);
         Assert.assertEquals(5,fibonacci);
     }
 
 
+    @Test
+    public void should_return_12586269025L_when_calculate_given_position_is_50(){
+        long number = 50;
+        long fibonacci = CalculationUtils.fibonacci(number);
+        Assert.assertEquals(12586269025L,fibonacci);
+    }
+
     @Test(expected = IllegalArgumentException.class)
     public void should_throw_IllegalArgumentException_when_calculate_given_position_less_1(){
-        int number = 0;
+        long number = 0;
+        CalculationUtils.fibonacci(number);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void should_throw_IllegalArgumentException_when_calculate_given_position_than_50(){
+        long number = 60;
         CalculationUtils.fibonacci(number);
     }
 }
